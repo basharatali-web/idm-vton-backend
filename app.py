315f8967c75cf3a172cb6d1dd875e360f14b9338
basharatali-web <softwaredivision92@gmail.com>
2026-06-10 12:31:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "IDM-VTON Backend Running"
+
+@app.route("/test")
+def test():
+    return jsonify({
+        "success": True,
+        "message": "Python backend working"
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
