a680f355dbf0f1e63736976a0f3427982aef6e33
basharatali-web <softwaredivision92@gmail.com>
2026-06-14 08:17:39 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from gradio_client import Client
+import traceback
 import os
 
 app = Flask(__name__)
@@ -7,26 +8,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "IDM-VTON Backend Running"
-
-
-@app.route("/test")
-def test():
-    return jsonify({
-        "success": True,
-        "message": "Python backend working"
-    })
-
-
-@app.route("/hf-test")
-def hf_test():
-
-    token = os.getenv("HF_TOKEN")
-
-    return jsonify({
-        "success": bool(token),
-        "message": "HF token found" if token else "HF token missing"
-    })
-
 
 @app.route("/space-test")
 def space_test():
@@ -44,17 +25,10 @@ def space_test():
 
         return jsonify({
             "success": False,
-            "error": str(e)
+            "error": str(e),
+            "type": str(type(e)),
+            "trace": traceback.format_exc()
         })
-
-
-@app.route("/tryon")
-def tryon():
-    return jsonify({
-        "success": True,
-        "message": "Try-On endpoint ready"
-    })
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
